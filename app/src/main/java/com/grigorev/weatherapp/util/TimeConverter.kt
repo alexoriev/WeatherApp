0822b08d41e4778e9839962a1aa8263806a79b20
aleksandrgrigorev <xundr1992@gmail.com>
2023-07-19 16:11:41 +0400

    Time converter style fix

--- app/src/main/java/com/grigorev/weatherapp/util/TimeConverter.kt
+++ app/src/main/java/com/grigorev/weatherapp/util/TimeConverter.kt
@@ -4,7 +4,7 @@ import java.util.Locale
 
 class TimeConverter {
     fun formatUnixTimeToDateTime(dateTime: Int): String {
-        val sdf = java.text.SimpleDateFormat("MMMM dd, yyyy 'at' HH:mm ", Locale.US)
+        val sdf = java.text.SimpleDateFormat("EEE, MMMM dd 'at' HH:mm ", Locale.US)
         val date = java.util.Date(dateTime * 1000L)
         return sdf.format(date)
     }
